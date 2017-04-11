3baa5745544c36a670c03e98a66bd84298711eff
nussam <nussam@nustechnology.com>
2017-04-11 14:12:35 +0700

    update
    gem
    view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,28 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
+//= require jquery.1.11.1
+//= require jquery.prettyPhoto
+//= require bootstrap.min
+//= require SmoothScroll
+//= require easypiechart
+//= require jquery.isotope
+//= require jquery.counterup
+//= require waypoints
+//= require jqBootstrapValidation
+//= require contact_me
+//= require main
 //= require jquery_ujs
 //= require turbolinks
-//= require SmoothScroll
-//= require bootstrap.min
 //= require cable
-//= require contact_me
-//= require easypiechart
-//= require jqBootstrapValidation
-//= require jquery.1.11.1
-//= require jquery.counterup
-//= require jquery.isotope
-//= require jquery.prettyPhoto
-//= require main
-//= require waypoints
 //= require_tree .
+//= require_self
+
+
