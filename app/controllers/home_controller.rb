1a30d5b3de13fb98e877bc345bbb156a3969f9ae
nussam <nussam@nustechnology.com>
2017-04-17 17:19:41 +0700

    update css and doc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/camera.pdf",
+      filename: "camera.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,11 @@ Rails.application.routes.draw do
     get "/check_username_exists", to: "sessions#check_username_exists"
   end
 
+  resources :home do
+    collection do
+      get :download_pdf
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
